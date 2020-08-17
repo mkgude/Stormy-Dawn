4940f42035ca8ad4875acc2ed4af76a1253a51ca
Marian Gude <mkgude@gmail.com>
2020-08-16 19:56:34 -0500

    made updates to new project html file

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 module.exports = {
   development: {
     username: "root",
-    password: "mysqlpassword",
+    password: process.env.DB_PASSWORD,
     database: "stormy_dawn",
     host: "127.0.0.1",
     dialect: "mysql"
