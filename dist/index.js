050274b4347c1a182c0d5cf804f2460ea9f2b5b7
Marian Gude <mkgude@gmail.com>
2020-09-24 09:39:22 -0700

    added links and photos to readme

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,56 @@
-$(document).ready(() => { function a(a, b, c, d, e, f, g) { $.post("/api/projects", { title: a, name: b, description: c, language: d, cause: e, website: f, location: g }).then(() => { window.location.replace("/blog") }).catch(a => { console.log(a) }) } const b = $("input#title"), c = $("input#name"), d = $("#description"), e = $("#language"), f = $("input#cause"), g = $("input#website"), h = $("input#location"); $("#submitBtn").click(i => { i.preventDefault(), console.log("Form submitted"); const j = { title: b.val().trim(), name: c.val().trim(), description: d.val().trim(), language: e.val().trim(), cause: f.val().trim(), website: g.val().trim(), location: h.val().trim() }; j.title && j.name && (a(j.title, j.name, j.description, j.language, j.cause, j.website, j.location), b.val(""), c.val(""), d.val(""), e.val(""), f.val(""), g.val(""), h.val("")) }) });
+$(document).ready(() => {
+  function a(a, b, c, d, e, f, g) {
+    $.post("/api/projects", {
+      title: a,
+      name: b,
+      description: c,
+      language: d,
+      cause: e,
+      website: f,
+      location: g
+    })
+      .then(() => {
+        window.location.replace("/blog");
+      })
+      .catch(a => {
+        console.log(a);
+      });
+  }
+  const b = $("input#title"),
+    c = $("input#name"),
+    d = $("#description"),
+    e = $("#language"),
+    f = $("input#cause"),
+    g = $("input#website"),
+    h = $("input#location");
+  $("#submitBtn").click(i => {
+    i.preventDefault(), console.log("Form submitted");
+    const j = {
+      title: b.val().trim(),
+      name: c.val().trim(),
+      description: d.val().trim(),
+      language: e.val().trim(),
+      cause: f.val().trim(),
+      website: g.val().trim(),
+      location: h.val().trim()
+    };
+    j.title &&
+      j.name &&
+      (a(
+        j.title,
+        j.name,
+        j.description,
+        j.language,
+        j.cause,
+        j.website,
+        j.location
+      ),
+      b.val(""),
+      c.val(""),
+      d.val(""),
+      e.val(""),
+      f.val(""),
+      g.val(""),
+      h.val(""));
+  });
+});
